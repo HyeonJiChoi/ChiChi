006c93d965230a70f2a2943fd5051b079313b0d0
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2019-10-25 17:45:23 +0900

    add questions
    add questions

---
+++ CHICHI/app/src/main/java/org/dementia/chichi/InspectorTestProblemQuestion.java
@@ -0,0 +1,21 @@
+package org.dementia.chichi;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InspectorTestProblemQuestion extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inspector_test_problem_question, container, false);
+    }
+
+}
--- CHICHI/app/src/main/java/org/dementia/chichi/InspectorTestScreen.java
+++ CHICHI/app/src/main/java/org/dementia/chichi/InspectorTestScreen.java
@@ -1,6 +1,7 @@
 package org.dementia.chichi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class InspectorTestScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspector_test_screen);
+        setActionbar();
+    }
+    public void setActionbar() {
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
+        setSupportActionBar(tb) ;
     }
 }
